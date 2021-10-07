b8f3a25757fabe3a3ffd6f39097d98d2a1432af7
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-10-07 16:05:24 -0300

    03-Methods and computed

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -4,14 +4,20 @@
         <div>Occupation: {{ occupation }}</div>
         <div>Bilingual: {{ lang }}</div>
         <div>Age: {{ age }}</div>
-        <button @click="changeName">Change name</button>
+        <button @click="changeName('SOME ARG',$event)">Change name</button>
+        <button @click="changeOccupation('GAMER')">Change occupation</button>
+
+        <hr/>
+        <div>
+            {{ message }}
+        </div>
     </div>
 </template>
 
 
 <script>
      /* eslint-disable */
-    import { ref } from 'vue';
+    import { ref, computed } from 'vue';
 
     export default {
         setup(){
@@ -19,22 +25,34 @@
             const occupation = ref('DEV');
             const bilingual = ref(false);
 
+            /// FUNCTIONS
+            function changeName(arg1, event){
+                console.log(arg1);
+                console.log(event)
+                name.value = 'Ronald';
+            }
+            const changeOccupation = (arg) => {
+                occupation.value = arg;
+            }
+            // COMPUTED
+            const message = computed(()=>{
+                return `Hello, i am ${name.value} and i am a ${occupation.value}`
+            })
+
+
             return {
                 name: name,
                 occupation,
-                lang: bilingual
+                lang: bilingual,
+                changeName,
+                changeOccupation,
+                message
             }            
         },
         data(){
             return {
                 age:18
             }
-        },
-        methods:{
-            changeName(){
-                this.name = 'Steve'
-            }
         }
-
     }
 </script>
