c4c1ecdef637cb76d57a1147db0da3742641fd1f
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-10-07 18:39:03 -0300

    08-Lifecycle hooks

--- src/components/Home/user.vue
+++ src/components/Home/user.vue
@@ -9,18 +9,71 @@
 </template>
 
 <script>
-    import { watch } from 'vue';
     /* eslint-disable */
+    import { 
+      onBeforeMount,
+      onMounted,
+      onBeforeUpdate,
+      onUpdated,
+      onRenderTracked,
+      onRenderTriggered,
+      watch,
+    } from 'vue';
+
     export default {
       props:['user'],
       setup(props,context){
-        console.log(context);
-
+        // console.log(context);
         watch(props.user,()=>{
           console.log('From the children');
           context.emit('saySomething',{ someArg: true})
         })
 
+        // beforeCreate
+        // created
+
+        onBeforeMount(()=>{
+          console.log('1-onBeforeMount')
+        })
+
+        onMounted(()=>{
+          console.log('2-onMounted')
+        })
+
+        onBeforeUpdate(()=>{
+          console.log('x-onBeforeUpdate')
+        })
+
+        onUpdated(()=>{
+          console.log('x-onUpdated')
+        })
+
+        // onBeforeUnmount
+        // onUnmounted
+
+
+        // onRenderTracked(({key, target})=>{
+        //   console.log(key);
+        //   console.log(target)
+        //   console.log("Render tracked !!")
+        // })
+
+        // onRenderTriggered(({key, target,oldValue,newValue})=>{
+        //   console.log(key);
+        //   console.log(target);
+
+        //   console.log(oldValue,'OLD');
+        //   console.log(newValue,'NEW');
+        //   console.log("Render tracked !!")
+        // })
+      },
+      renderTriggered({key, target,oldValue,newValue}){
+          console.log(key);
+          console.log(target);
+
+          console.log(oldValue,'OLD');
+          console.log(newValue,'NEW');
+          console.log("Render tracked !!")
       }
     }
 </script>
