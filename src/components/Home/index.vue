fd8ae8bffa40382ddc930ca92fb8d6196069e54d
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-10-07 15:48:57 -0300

    02-refs

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,15 +1,40 @@
 <template>
     <div>
-        HOME
+        <div>Hello my name is: {{ name }}</div>
+        <div>Occupation: {{ occupation }}</div>
+        <div>Bilingual: {{ lang }}</div>
+        <div>Age: {{ age }}</div>
+        <button @click="changeName">Change name</button>
     </div>
 </template>
 
 
 <script>
-    /* eslint-disable */
+     /* eslint-disable */
+    import { ref } from 'vue';
+
     export default {
         setup(){
+            const name = ref('Francis');
+            const occupation = ref('DEV');
+            const bilingual = ref(false);
 
+            return {
+                name: name,
+                occupation,
+                lang: bilingual
+            }            
+        },
+        data(){
+            return {
+                age:18
+            }
+        },
+        methods:{
+            changeName(){
+                this.name = 'Steve'
+            }
         }
+
     }
 </script>
