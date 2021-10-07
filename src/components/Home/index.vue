735128bb5bc042f6464ae40c98823d6649d66e51
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-10-07 19:10:30 -0300

    09-provide and inject

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -25,16 +25,17 @@
 <script>
      /* eslint-disable */
     import appUser from './user.vue';
-    import { ref, computed, watch, reactive } from 'vue';
+    import { ref, computed, watch, reactive, provide, readonly } from 'vue';
 
     export default {
         components:{
             appUser
         },
         setup(){
+            const toProvide = 'Some data to provide';
             const form = reactive({
-                name:'',
-                lastname:''
+                name:'Steve',
+                lastname:'Thompson'
             });
 
             const user = reactive({
@@ -44,9 +45,17 @@
                 bilingual:false
             });
 
+            /// PROVIDE
+            provide('data',toProvide);
+            provide('formData',readonly(form));
+            provide('mutateForm',mutateForm)
+
             /// FUNCTIONS
             function changeName(arg1, event){
                 user.name = 'Ronald';
+            }
+            function mutateForm(value){
+                form.name = value
             }
             const changeOccupation = (arg) => {
                 user.occupation = arg;
@@ -58,6 +67,8 @@
             const fromEmit = (arg) => {
                 console.log('FROM EMMIT !!',arg)
             }
+          
+
 
             // COMPUTED
             const message = computed(()=>{
--- src/components/Home/user.vue
+++ src/components/Home/user.vue
@@ -1,79 +1,43 @@
 <template>
-  <div>
-          <div>Hello my name is: {{ user.name }}</div>
-          <div>Occupation: {{ user.occupation }}</div>
-          <div>Bilingual: {{ user.bilingual }}</div>
-          <div>Age: {{ user.age }}</div>
-          <slot></slot>
-  </div>
+<div>
+    <div>Hello my name is: {{ user.name }}</div>
+    <div>Occupation: {{ user.occupation }}</div>
+    <div>Bilingual: {{ user.bilingual }}</div>
+    <div>Age: {{ user.age }}</div>
+    <slot></slot>
+    <div>
+      Name: {{ formData.name }},Lastname:{{ formData.lastname}}
+    </div>
+</div>
 </template>
 
 <script>
-    /* eslint-disable */
-    import { 
-      onBeforeMount,
-      onMounted,
-      onBeforeUpdate,
-      onUpdated,
-      onRenderTracked,
-      onRenderTriggered,
-      watch,
-    } from 'vue';
+/* eslint-disable */
+import {
+    inject,
+    onMounted,
+    watch,
+} from 'vue';
 
-    export default {
-      props:['user'],
-      setup(props,context){
-        // console.log(context);
-        watch(props.user,()=>{
-          console.log('From the children');
-          context.emit('saySomething',{ someArg: true})
-        })
+export default {
+    props: ['user'],
+    setup(props, context) {
+      const data = inject('data');
+      const formData = inject('formData');
+      const mutateForm = inject('mutateForm');
 
-        // beforeCreate
-        // created
+      onMounted(()=>{
+        formData.name = 'something';
 
-        onBeforeMount(()=>{
-          console.log('1-onBeforeMount')
-        })
+        // setTimeout(()=>{
+        //   mutateForm('MIKE');
+        // },2000)
+      })
 
-        onMounted(()=>{
-          console.log('2-onMounted')
-        })
-
-        onBeforeUpdate(()=>{
-          console.log('x-onBeforeUpdate')
-        })
-
-        onUpdated(()=>{
-          console.log('x-onUpdated')
-        })
-
-        // onBeforeUnmount
-        // onUnmounted
-
-
-        // onRenderTracked(({key, target})=>{
-        //   console.log(key);
-        //   console.log(target)
-        //   console.log("Render tracked !!")
-        // })
-
-        // onRenderTriggered(({key, target,oldValue,newValue})=>{
-        //   console.log(key);
-        //   console.log(target);
-
-        //   console.log(oldValue,'OLD');
-        //   console.log(newValue,'NEW');
-        //   console.log("Render tracked !!")
-        // })
-      },
-      renderTriggered({key, target,oldValue,newValue}){
-          console.log(key);
-          console.log(target);
-
-          console.log(oldValue,'OLD');
-          console.log(newValue,'NEW');
-          console.log("Render tracked !!")
+      return {
+        data,
+        formData
       }
     }
+}
 </script>
